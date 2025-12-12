4ae9b3b52882074f7f30ba63d0027dabc4c36352
jimi-64 <237873197+jimi-64@users.noreply.github.com>
2025-12-12 20:14:47 +0100

    copper tools, armor, lanterns and torches are getting no longer blocked

--- src/client/java/com/jimi64/copperblocker/client/CopperblockerClient.java
+++ src/client/java/com/jimi64/copperblocker/client/CopperblockerClient.java
@@ -1,10 +1,42 @@
 package com.jimi64.copperblocker.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemStackSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class CopperblockerClient implements ClientModInitializer {
 
+    public static final String MOD_ID = "CopperBlocker";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static ArrayList<String> denyList = new ArrayList<String>();
+    public static String blockerMode = "both";
+
+
     @Override
     public void onInitializeClient() {
+
+        // generate the denyList
+        String[] oxidizationPrefixes = {"minecraft:", "minecraft:exposed_", "minecraft:weathered_", "minecraft:oxidized_"};
+        String[] blockVariants = {"copper", "chiseled_copper", "copper_grate", "cut_copper", "cut_copper_stairs", "cut_copper_slab", "copper_bars", "copper_door", "copper_trapdoor", "copper_bulb", "copper_chain", "copper_lantern", "lightning_rod", "copper_chest", "copper_golem_statue"};
+
+        for (String oxidizationPrefix : oxidizationPrefixes) {
+            for (String blockVariant : blockVariants) {
+
+                denyList.add(oxidizationPrefix + blockVariant);
+
+            }
+        }
+
+        // changes the entry for the copper block
+        // because it doesn't follow the naming convention
+        denyList.set(0, "minecraft:copper_block");
+
+        LOGGER.info("Loaded the deny list!");
     }
 }
--- src/client/java/com/jimi64/copperblocker/mixin/client/CopperBlockerMixin.java
+++ src/client/java/com/jimi64/copperblocker/mixin/client/CopperBlockerMixin.java
@@ -2,32 +2,57 @@ package com.jimi64.copperblocker.mixin.client;
 
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import net.minecraft.item.ItemStackSet;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Collection;
+import java.util.function.Supplier;
+
+import static com.jimi64.copperblocker.client.CopperblockerClient.denyList;
+import static com.jimi64.copperblocker.client.CopperblockerClient.LOGGER;
 
 @Mixin(value = ItemGroup.class)
 public abstract class CopperBlockerMixin {
 
-    @Shadow
-    private Collection<ItemStack> displayStacks;
 
-    @Inject(method = "getDisplayStacks", at = @At("HEAD"))
-    void getDisplayStacks(CallbackInfoReturnable<Collection<ItemStack>> cir) {
+    @Shadow private Collection<ItemStack> displayStacks;
+    @Shadow @Final private Text displayName;
+    @Unique private Collection<ItemStack> filteredDisplayStacks;
 
-        //Collection<ItemStack> displayStacks = this.displayStacks.clone();
 
-        for (ItemStack displayStack : this.displayStacks) {
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
+    private void ItemGroup(ItemGroup.Row row, int column, ItemGroup.Type type, Text displayName, Supplier iconSupplier, ItemGroup.EntryCollector entryCollector, CallbackInfo ci) {
+        this.filteredDisplayStacks = ItemStackSet.create();
+    }
 
-            String itemName = displayStack.getName().toString();
 
-            if ((itemName.contains("copper") || itemName.contains("lightning_rod")) && !(itemName.contains("waxed"))) {
-                this.displayStacks.remove(displayStack);
+    /**
+     * @author jimi64
+     * @reason prevents all items on the denyList from being shown in the creative menu
+     */
+    @Overwrite
+    public Collection<ItemStack> getDisplayStacks() {
+
+        if (this.filteredDisplayStacks.isEmpty()) {
+
+            // copies all entries of displayStacks into filteredDisplayStacks
+            this.filteredDisplayStacks.addAll(this.displayStacks);
+
+            for (ItemStack displayStack : this.filteredDisplayStacks) {
+
+                // gets the item name of the displayStack object
+                String itemName = displayStack.getItem().toString();
+
+                if (denyList.contains(itemName)) {
+                    this.filteredDisplayStacks.remove(displayStack);
+                }
             }
+            LOGGER.info("Filtered ItemGroup '" + this.displayName.getString() + "' successfully!");
         }
+        return this.filteredDisplayStacks;
     }
 }
